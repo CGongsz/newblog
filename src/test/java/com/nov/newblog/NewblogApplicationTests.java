a06e4084693a6f55e003686e9c6e3d7803cc9666
nov <354597202@qq.com>
2020-03-11 13:02:41 +0800

    Initial commit

---
+++ src/test/java/com/nov/newblog/NewblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nov.newblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
